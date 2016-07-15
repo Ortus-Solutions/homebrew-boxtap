57a91755375d28c9d6e3fa7e0c00895a77181bd9
Jon Clausen <jon_clausen@silowebworks.com>
2016-07-15 11:22:39 -0400

    Bump devel

--- commandbox.rb
+++ commandbox.rb
@@ -1,12 +1,12 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
-  homepage "http://www.ortussolutions.com/products/commandbox"
+  homepage "https://www.ortussolutions.com/products/commandbox"
   url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.1.1/commandbox-bin-3.1.1.zip"
   sha256 "050bb737db3033a7ae8d47f2e5a973583963328c7ed1c3e1ab7b1191c609ac60"
 
   devel do
-    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.1/commandbox-bin-3.1.1.zip"
-    sha256 "050bb737db3033a7ae8d47f2e5a973583963328c7ed1c3e1ab7b1191c609ac60"
+    url "https://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.2.0-snapshot/commandbox-bin-3.2.0-snapshot.zip"
+    sha256 "f4ab2733629f0ff9b0ed2efc17e4a6f219718d5da1a7d6f1fae686f9d4f55945"
   end
 
   bottle :unneeded
