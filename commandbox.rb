d669119ef136ea45b270b247fe245db366fd83fe
Jon Clausen <jon_clausen@silowebworks.com>
2016-07-26 08:15:07 -0400

    Change download URL

--- commandbox.rb
+++ commandbox.rb
@@ -1,11 +1,11 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
   homepage "https://www.ortussolutions.com/products/commandbox"
-  url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.2.0/commandbox-bin-3.2.0.zip"
+  url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.2.0/commandbox-bin-3.2.0.zip"
   sha256 "42b0be0f5bcfdae161e24a587c98f8a21550ad76f23303cfb7f06eed03cbd8c5"
 
   devel do
-    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.2.0/commandbox-bin-3.2.0.zip"
+    url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.2.0/commandbox-bin-3.2.0.zip"
     sha256 "42b0be0f5bcfdae161e24a587c98f8a21550ad76f23303cfb7f06eed03cbd8c5"
     version "3.2.0"
   end
@@ -15,7 +15,7 @@ class Commandbox < Formula
   depends_on :java => "1.7+"
 
   resource "apidocs" do
-    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.2.0/commandbox-apidocs-3.2.0.zip"
+    url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.2.0/commandbox-apidocs-3.2.0.zip"
     sha256 "7444852e09d10f04cd087ade4e4962e4abb0b0a8a1e0b63beba971bf2c563a3c"
   end
 
