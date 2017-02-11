2c191781f9306633cdf0a3b0dda8b95b14e842fb
root <root@stg.ortussolutions.com>
2017-02-10 21:05:13 -0600

    CommandBox Build #00624: Release @ 3.5.0/Devel @ 3.5.1-snapshot

--- commandbox.rb
+++ commandbox.rb
@@ -5,8 +5,8 @@ class Commandbox < Formula
   sha256 "9477d83616124833f3e95b9e47391c4e61cfe4197b7d02d2d83b92e85219702c"
 
   devel do
-    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.5.1-snapshot/commandbox-bin-3.5.1-snapshot.zip?build=00623"
-    sha256 "47eda64f9ce39c6ae9ca0a9249ad61dc54142d43a87aaaf21b0b4210a43565c1"
+    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.5.1-snapshot/commandbox-bin-3.5.1-snapshot.zip?build=00624"
+    sha256 "dea4536c7701946e6f756a9f1fe90c89d506620b76fe421598d60f26c602bd16"
   end
 
   bottle :unneeded
