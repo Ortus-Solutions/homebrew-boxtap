c1445537250bd00554301bbb0cd4294dd5db1fbd
root <root@stg.ortussolutions.com>
2018-06-26 22:54:52 -0500

    CommandBox Build #01034: Release @ 4.1.0/Devel @ 4.1.1-alpha

--- commandbox.rb
+++ commandbox.rb
@@ -2,11 +2,11 @@ class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
   homepage "https://www.ortussolutions.com/products/commandbox"
   url "https://downloads.ortussolutions.com/ortussolutions/commandbox/4.1.0/commandbox-bin-4.1.0.zip"
-  sha256 "a17d21431d62bf45ba4d712bc310f7acf4644cc25609934bf13b65e1bb1f2132"
+  sha256 "b67317451253d239a225cea158cbc12aedef3e5bf94580503cfa2503f136266b"
 
   devel do
-    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/4.1.1-alpha/commandbox-bin-4.1.1-alpha.zip?build=00041"
-    sha256 "10f34e01b3273dbcbf72be330a8ae390b6b0730af2d843a15fe8c6597f2eb4a2"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/4.1.1-alpha/commandbox-bin-4.1.1-alpha.zip?build=01034"
+    sha256 "4b5a1e8bfbb9c99f366088cf4798d3c9bd9f59acabe8475e5e1cb0bb9ea3b87e"
   end
 
   bottle :unneeded
@@ -15,7 +15,7 @@ class Commandbox < Formula
 
   resource "apidocs" do
     url "https://downloads.ortussolutions.com/ortussolutions/commandbox/4.1.0/commandbox-apidocs-4.1.0.zip"
-    sha256 "cb093aeb7ae7233f6c3fbf96039e2b289a41462a8d48c86bc03bdd3dfbd6ec34"
+    sha256 "ababed851aba087de3abc532ae999c48e155af74a933ff2223171ed59a3201a3"
   end
 
   def install
