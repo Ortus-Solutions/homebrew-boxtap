7272b6aebaa04d10dcd8e2f91236164ccc48e554
Jenkins <jenkins@OrtusHQ-CI.ortus.local>
2020-07-08 15:17:07 -0500

    CommandBox Build #00213: Release @ 5.1.1/Devel @ 5.2.0-alpha

--- commandbox.rb
+++ commandbox.rb
@@ -5,8 +5,8 @@ class Commandbox < Formula
   sha256 "0b6b358ae2466297a7ddbd16d72a8b8a83c66d3f6cada0c8ff1e786d2a88af8b"
 
   devel do
-    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.2.0-alpha/commandbox-bin-5.2.0-alpha.zip?build=00212"
-    sha256 "cdfe3abf84e5d03cd2ca279af2e91a6293fb24c1c7e24a9a6d6841b22e1bb1c9"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.2.0-alpha/commandbox-bin-5.2.0-alpha.zip?build=00213"
+    sha256 "480934518b0835b55fb28e282f6e23e1b218d7c811b37e6fea01c6eb468c4c86"
   end
 
   bottle :unneeded
