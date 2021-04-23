050c6f1022a8336c4111bcbbfb47852a62e63b2b
Jenkins <jenkins@ci.ortushq.com>
2021-04-23 19:05:15 +0000

    CommandBox Build #00347: Release @ 5.2.1/Devel @ 5.3.0-alpha

--- commandbox.rb
+++ commandbox.rb
@@ -6,8 +6,8 @@ class Commandbox < Formula
   license "LGPL-3.0-or-later"
 
   head do
-    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.3.0-alpha/commandbox-bin-5.3.0-alpha.zip?build=00346"
-    sha256 "dc5b05680517885a929c041f0c222c74064193b2238306f603e2003eab63483d"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.3.0-alpha/commandbox-bin-5.3.0-alpha.zip?build=00347"
+    sha256 "587c8c54514a8911c2dc76c23ba543551ba3e7be9535ad1b7208617195d66bf5"
   end
 
   livecheck do
