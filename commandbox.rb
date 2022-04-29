5f2077dcf49e5e19d48b5c9042e1ce8d09341841
Jenkins <jenkins@ci.ortushq.com>
2022-04-29 05:06:28 +0000

    CommandBox Build #00558: Release @ 5.5.0/Devel @ 5.5.0

--- commandbox.rb
+++ commandbox.rb
@@ -1,13 +1,13 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
   homepage "https://www.ortussolutions.com/products/commandbox"
-  url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.4.2/commandbox-bin-5.4.2.zip"
-  sha256 "88de96d481d32fd8e42b18e12492d1d1cf6cb70a8a1368d192ba7e8f3da9829a"
+  url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.5.0/commandbox-bin-5.5.0.zip"
+  sha256 "6fef820e8366d40cb8c322a8d132d2bd46d4444ad28cfc41a57bbe0d7da0b343"
   license "LGPL-3.0-or-later"
 
   head do
-    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.5.0-alpha/commandbox-bin-5.5.0-alpha.zip?build=00557"
-    sha256 "9d2be5f1fb8d26dc6470abdec96946ca48359d388689a4f5e6cf558161981fb2"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.5.0/commandbox-bin-5.5.0.zip?build=00558"
+    sha256 "6fef820e8366d40cb8c322a8d132d2bd46d4444ad28cfc41a57bbe0d7da0b343"
   end
 
   livecheck do
@@ -23,8 +23,8 @@ class Commandbox < Formula
   depends_on "openjdk@11"
 
   resource "apidocs" do
-    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.4.2/commandbox-apidocs-5.4.2.zip"
-    sha256 "66c040230f3a2e2d3b03023d895416b271e62a7da264f25e3df02040099e67fc"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.5.0/commandbox-apidocs-5.5.0.zip"
+    sha256 "d6a1c261154c1f898c0f93dc2ee210603f4e22a3ed657bba595c17a5127202b4"
   end
 
   def install
